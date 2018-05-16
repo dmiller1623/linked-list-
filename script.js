aa948794d89b6b6675a74197715d359c8dfde42a
Dennis <dmiller1623@gmail.com>
2018-05-16 10:44:56 -0600

    updated javascript

--- script.js
+++ script.js
@@ -3,38 +3,60 @@
 var submitButton = document.querySelector('#submit-bookmark');
 var userInputTitle = document.querySelector('#web-title');
 var userInputUrl = document.querySelector('#user-url');
-var bookmarksDisplay = document.querySelector('#bookmarks-display');
+var bookmarksDisplay = $('#bookmarks-display');
 var bookmarkForm = document.querySelector('#bookmark-form');
-var bookmarkArray = [];
-var arrayLength = bookmarkArray.length;
 bookmarkForm.addEventListener('submit', function(e) {
   e.preventDefault();
-  createBookmark();
+  displayBookmarks();
 })
 
-function createBookmark() {
-  var bookmark = new Bookmark(userInputTitle.value, userInputUrl.value);
-  bookmarkArray.push(bookmark);
-  console.log(bookmarkArray[0]);
-  displayBookmarks();
-}
 
 function displayBookmarks() {
-  var title = bookmarkArray[0].title
-  var url = bookmarkArray[0].url
-  bookmarkArray{} ++;
-  var template = ` <article class="new-bookmark">
-    <h2 class="title-header">${title}</h2>
-    <p class ="link-header">${url}</p>
-    <button class="read">Read</button>
-    <button class="delete">Delete</button>
-  </article> `;
-  bookmarksDisplay.innerHTML = template;
+  var bookmark = new Bookmark(userInputTitle.value, userInputUrl.value);
+  var title = bookmark.title
+  var url = bookmark.url
+  var template = (` 
+    <article class="new-bookmark">
+      <h2 class="title-header">${title}</h2>
+      <a class="link-header" href ="${'https://' + url }">${url}</a>
+      <button class="readbutton">Read</button>
+      <button class="deletebutton">Delete</button>
+    </article> 
+  `);
+  bookmarksDisplay.append(template);
+  missingInput();
+  changeReadButton();
+  deleteBookmark();
 }
 
+function missingInput() {
+  if (userInputUrl.value === "" || userInputTitle.value === ""){
+    alert('Missing Input');
+  }
+}
 
 function Bookmark(title, url) {
   this.title = title;
   this.url = url;
-}
+ }
 
+ function deleteBookmark() {
+  var parentBox = document.querySelector('.new-bookmark');
+  var deleteButton = document.querySelector('.deletebutton');
+  parentBox.addEventListener('click', function(event) {
+    if (event.target.className === "deletebutton") {
+    this.classList.add('delete')
+  }
+  })
+ }
+
+function changeReadButton() {
+  var parentBox = document.querySelector('.new-bookmark');
+  var buttonRead = document.querySelector('.readbutton');
+   parentBox.addEventListener('click', function(event) {
+      if (event.target.className === "readbutton") {
+      this.classList.add('new-read')
+      console.log(this);
+      }
+})
+}    
